d2c7623bb6edc35d7756ea2e86520f2fcd27c412
QIANGUOFENG31396 <38576987+QIANGUOFENG31396@users.noreply.github.com>
2022-04-20 11:11:48 +0800

    Add files via upload

---
+++ help.py
@@ -0,0 +1,27 @@
+
+# a = [1,2,3,4,5]
+# b = {1,2,3,4,5}
+# # c = (1,2,3,4,5）
+
+# print("a类型 =")
+# print(type(a))
+
+
+# print("b类型 =")
+# print(type(b))
+
+# help(max)
+# help(len)
+
+# a = [1,2,3,5,6]
+# b = len(a)
+# print(a)
+# print(b)
+# # for x in range(10,0,-1):
+# #     print(x)
+
+# help(input)
+
+a = input("input1")
+print(a)
+a = input("input2")
